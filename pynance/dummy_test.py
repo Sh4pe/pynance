364352f364d40ed2bbb08cc745bc2e32d7880e30
David Nies <david.nies@posteo.de>
2019-02-01 00:26:37 +0100

    Add CSV importer
    All code is written by @fa-me.

--- pynance/dummy_test.py
+++ pynance/dummy_test.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 
 import unittest
+
 from pynance.dummy import my_dummy_function
+
 
 class DummyTestCase(unittest.TestCase):
     def test_my_dummy_function(self):
         self.assertEqual(my_dummy_function(2), 4)
         self.assertEqual(my_dummy_function(4), 8)
+
 
 def test_suite():
     suite = unittest.TestSuite()
