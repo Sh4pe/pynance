028e865713a3ad3b8a345fec4eb661ce3f963c3e
David Nies <david.nies@posteo.de>
2019-02-03 14:49:03 +0100

    Add dummy docstring to show that `doctest` works

--- pynance/dummy.py
+++ pynance/dummy.py
@@ -4,4 +4,10 @@ This is just a placeholder.
 """
 
 def my_dummy_function(number):
+    """
+    Dummy function that doubles its input.
+
+    >>> my_dummy_function(2)
+    4
+    """
     return 2*number
