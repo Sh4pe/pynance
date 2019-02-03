649b4ff54ee882c262a2ca7254dcdfbbc7e02616
Fabian Meyer <fabian.meyer@mailbox.org>
2019-02-03 23:12:23 +0100

    added tests, extended unittests

--- pynance/dash_viz/plot_flow_test.py
+++ pynance/dash_viz/plot_flow_test.py
@@ -1,8 +1,10 @@
+
 from __future__ import print_function, absolute_import
 
 import unittest
 
-from .plot_flow import app, csvtype_string2description
+from .plot_flow import app, csvtype_string2description, update_output,\
+    onselect_csvtype, update_csvtype_store
 from ..textimporter import SupportedCsvTypes
 
 # see https://github.com/plotly/dash/issues/297
@@ -17,3 +19,20 @@ class DashTestCase(unittest.TestCase):
                          csvtype_string2description('DKBCash'))
         self.assertEqual(SupportedCsvTypes.DKBVisa,
                          csvtype_string2description('DKBVisa'))
+
+    def test_update_output_None(self):
+        self.assertFalse(update_output(None, "") is None)
+
+    def test_onselect_csvtype(self):
+        dropdown_values = [None, "DKBCash", "DKBVisa"]
+        onselect_response = [False, True, True]
+
+        for expected, selected in zip(onselect_response, dropdown_values):
+            result = onselect_csvtype(selected)
+            # TODO: how to test if propagation of value change
+            # is performed correctly?
+
+
+def test_suite():
+    suite = unittest.makeSuite(DashTestCase)
+    return suite
--- unittests.py
+++ unittests.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
+from __future__ import print_function, absolute_import
 
 import unittest
 import pynance.dummy_test
 import pynance.textimporter_test
+import pynance.dash_viz.plot_flow_test
 
 
 def test_suite():
     suite = unittest.TestSuite()
     suite.addTests(pynance.dummy_test.test_suite())
     suite.addTests(pynance.textimporter_test.test_suite())
+    suite.addTests(pynance.dash_viz.plot_flow_test.test_suite())
 
     return suite
 
@@ -16,6 +19,7 @@ def test_suite():
 def run_all_unit_tests():
     test_runner = unittest.TextTestRunner()
     return len(test_runner.run(test_suite()).failures) == 0
+
 
 if __name__ == "__main__":
     run_all_unit_tests()
