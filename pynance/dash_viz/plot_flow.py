da269ef8eaeee7326e4e3e05502f6fdd893809c3
Fabian Meyer <fabian.meyer@mailbox.org>
2019-02-03 17:57:20 +0100

    working chart shows incoming/outcoming data for DKB-Cash files

--- pynance/dash_viz/plot_flow.py
+++ pynance/dash_viz/plot_flow.py
@@ -3,6 +3,7 @@ from dash.dependencies import Input, Output, State
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_table
+import plotly.graph_objs as go
 import base64
 import io
 
@@ -14,13 +15,13 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div([
     dcc.Upload(
-        id='upload-data',
+        id='uploader',
         children=html.Div([
             'Drag and Drop or ',
             html.A('Select Files')
         ]),
         style={
-            'width': '100%',
+            'width': '300px',
             'height': '60px',
             'lineHeight': '60px',
             'borderWidth': '1px',
@@ -32,46 +33,58 @@ app.layout = html.Div([
         # Allow multiple files to be uploaded
         multiple=False
     ),
-    html.Div(id='output-data-upload'),
+    dcc.Graph(
+        figure=go.Figure(
+            data=[],
+            ),
+        style={'height': 500},
+        id='my-graph'
+    ),
+    html.Div(id='output-data-upload')
 ])
 
-
-def parse_contents(contents, filename, date):
+def parse_contents(contents):
     content_type, content_string = contents.split(',')
+    decoded = base64.b64decode(content_string)
+    
+    csvtype = SupportedCsvTypes.DKBCash
+    encoding = csvtype.encoding
 
-    try:
-        if 'csv' in filename:
-            decoded = base64.b64decode(content_string)
-            
-            csvtype = SupportedCsvTypes.DKBCash
-            encoding = csvtype.encoding
+    return read_csv(io.StringIO(decoded.decode(encoding)), csvtype)
 
-            print(decoded[:500])
-
-            df = read_csv(io.StringIO(decoded.decode(encoding)), csvtype)
-            print(df.columns)
-
-            return html.Div([
-                    dash_table.DataTable(
-                        data=df.to_dict('rows'),
-                        columns=[{'name': i, 'id': i} for i in df.columns]
-                    )
-                ])
-        else:
-            return html.Div([
-                            'Unsupported file extension'
-                            ])
-    except Exception as e:
-        print(e)
-        return html.Div([
-            'There was an error processing this file.'
-        ])   
-
-
-@app.callback(Output('output-data-upload', 'children'),
-              [Input('upload-data', 'contents')],
-              [State('upload-data', 'filename'),
-               State('upload-data', 'last_modified')])
-def update_output(content, name, date):
+@app.callback(Output('my-graph', component_property='figure'),
+              [Input('uploader', component_property='contents')])
+def update_output(content):
     if content is not None:
-        return parse_contents(content, name, date)
+        df = parse_contents(content)
+
+        pos = df[df["amount"] > 0]
+        neg = df[df["amount"] < 0]
+
+        pos_bar = go.Bar(x=pos["date"],
+                         y=pos["amount"],
+                         text=pos["text"],
+                         name="incoming")
+        neg_bar = go.Bar(x=neg["date"],
+                         y=neg["amount"],
+                         text=neg["text"],
+                         name="outgoing")
+
+        fig = go.Figure(
+            data=[pos_bar, neg_bar],
+            layout=go.Layout(
+                title='flow',
+                showlegend=True,
+                legend=go.layout.Legend(
+                    x=0,
+                    y=1.0
+                ),
+                margin=go.layout.Margin(l=40, r=0, t=40, b=30)
+            )
+        )
+
+        return fig
+    else: return go.Figure(
+            data=[],
+            ),
+
