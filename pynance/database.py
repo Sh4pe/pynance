37b37a30ddca4e5db42f6dcec3c765566c7495b9
David Nies <david.nies@posteo.de>
2019-02-10 15:45:10 +0100

    Add class InsertTable
    This class duplicates a Pandas DataFrame into a temporary table
    inside a sqlite database. The table is disposed once it is not needed
    any more.

--- pynance/database.py
+++ pynance/database.py
@@ -2,17 +2,17 @@ import sqlite3
 
 class LowLevelConnection(object):
     """
-    Class that handles low-level database connection. Should be used in with-statements.
+    Class that handles low-level database connection. Makes sure the expected table strucutre exists.
+    Should be used in with-statements. 
     """
-
 
     # Schema evolution should be handled later once it is needed
     SUPPORTED_SCHEMA_VERSIONS = [1]
 
     TABLE_SCHEMA_VERSION = 'schema'
     TABLE_TRANSACTIONS = 'transactions'
+    TABLE_TRANSACTIONS_ID = 'id INTEGER PRIMARY KEY'
     TABLE_TRANSACTIONS_FIELDS = [
-        'id INTEGER PRIMARY KEY',
         'imported_at INTEGER', # unix timestamp
         'date TEXT', # format: YYYY-MM-DD
         'sender_account TEXT', 
@@ -43,8 +43,11 @@ class LowLevelConnection(object):
 
             cursor.execute('CREATE TABLE IF NOT EXISTS {} ({})'.format(
                 LowLevelConnection.TABLE_TRANSACTIONS,
-                ', '.join(LowLevelConnection.TABLE_TRANSACTIONS_FIELDS)
+                ', '.join(
+                    [LowLevelConnection.TABLE_TRANSACTIONS_ID] + LowLevelConnection.TABLE_TRANSACTIONS_FIELDS
+                )
             ))
+            cursor.execute('CREATE INDEX date_index ON {} ({})'.format(LowLevelConnection.TABLE_TRANSACTIONS, 'date'))
 
             cursor.execute('COMMIT')
             conn.commit()
@@ -55,3 +58,51 @@ class LowLevelConnection(object):
     
     def __exit__(self, _1, _2, _3):
         self.conn.close()
+
+
+class InsertTable(object):
+    """
+    This class makes sure that a DataFrame is inserted into a temporary table of a sqlite databases.
+    It also makes sure that the temporary table is created in a safe way and disposed afterwards. For
+    this purpuse, instances of this class should be used in with statements.
+    """
+    
+    @staticmethod
+    def create_temp_table(conn):
+        """Creates temporary table suitable for inserting the DataFrame and returns its name."""
+
+        cursor = conn.cursor()
+        i, table_name, go_on = 0, '', True
+
+        while go_on:
+            go_on = False
+            table_name = 'insert_df_{}'.format(i)
+            try:
+                cursor.execute('CREATE TEMPORARY TABLE {} ({})'.format( table_name, ', '.join(LowLevelConnection.TABLE_TRANSACTIONS_FIELDS)))
+            except sqlite3.OperationalError:
+                go_on = True
+                i += 1
+        
+        return 'temp', table_name
+
+    def __init__(self, conn, data_frame):
+        "uses conn, fetches everything from 'data_frame' into a temporary table"
+        
+        self.conn = conn
+        self.temp_table_schema, self.temp_table_name = InsertTable.create_temp_table(conn)
+        data_frame.to_sql(
+            name=self.temp_table_name, 
+            schema=self.temp_table_schema,
+            index=False,
+            con=conn, 
+            chunksize=5000
+        )
+    
+    def __enter__(self):
+        return (self.temp_table_schema, self.temp_table_name)
+    
+    def __exit__(self, _1, _2, _3):
+        "Make sure the table is gone."
+        self.conn.cursor().execute('DROP TABLE {}.{}'.format(
+            self.temp_table_schema, self.temp_table_name
+        ))
--- pynance/database_test.py
+++ pynance/database_test.py
@@ -2,8 +2,10 @@ import unittest
 import os.path
 import shutil
 from tempfile import mkdtemp
+import sqlite3
 
-from pynance.database import LowLevelConnection
+from pynance.database import LowLevelConnection, InsertTable
+from pynance.textimporter import read_csv, SupportedCsvTypes
 
 class TemporaryDirectory(object):
     def __enter__(self):
@@ -44,12 +46,76 @@ class LowLevelConnectionTestCase(unittest.TestCase):
                     cursor.execute('select count(*) from {}'.format(LowLevelConnection.TABLE_SCHEMA_VERSION)).fetchall()
                 )
 
+class InsertTableTestCase(unittest.TestCase):
+
+    def test_create_temp_table_table_exists(self):
+        with TemporaryDirectory() as tmp_dir:
+            with LowLevelConnection(1, os.path.join(tmp_dir, 'test.db')) as conn:
+                table_schema, table_name = InsertTable.create_temp_table(conn)
+                # Fails if and only if table does not exist
+                conn.cursor().execute('select count(*) from {}.{}'.format(table_schema, table_name))
+    
+    def test_create_temp_table_choses_other_table_if_exists(self):
+        with TemporaryDirectory() as tmp_dir:
+            with LowLevelConnection(1, os.path.join(tmp_dir, 'test.db')) as conn:
+                conn.cursor().execute('CREATE TEMPORARY TABLE insert_df_0 (id INT)')
+                table_schema, table_name = InsertTable.create_temp_table(conn)
+                self.assertEqual(table_schema, 'temp')
+                self.assertEqual(table_name, 'insert_df_1', 'expected table creation to fail exactly the first time')
+    
+    def test_it_removes_the_temporary_table(self):
+        test_data_frame = read_csv(os.path.join('pynance', 'test_data', 'dkb_cash_sample.csv'), SupportedCsvTypes.DKBCash)
+        # TODO: get rid of the 'drop' here
+        test_data_frame = test_data_frame.drop(['origin'], axis=1)
+        with TemporaryDirectory() as tmp_dir:
+            with LowLevelConnection(1, os.path.join(tmp_dir, 'test.db')) as conn:
+                insert_table_with_schema = ''
+
+                def check_if_table_exists():
+                    conn.cursor().execute('select count(*) from {}'.format(insert_table_with_schema))
+
+                with InsertTable(conn, test_data_frame) as insert_table:
+                    insert_table_with_schema = '{}.{}'.format(insert_table[0], insert_table[1])
+                    check_if_table_exists()
+                
+                self.assertRaises(sqlite3.OperationalError, check_if_table_exists)
+
+    def test_it_works_with_dataframes_from_text_importer(self):
+        def run_test(csv_file, df_format):
+            # Get the DataFrame
+            self.assertTrue(os.path.isfile(csv_file))
+            # TODO: Investigate what origin is good for and if we want to include it as column
+            # in the database as well.
+            data_frame = read_csv(csv_file, df_format).drop(['origin'], axis=1)
+            self.assertTrue(len(data_frame.index) > 0)
+    
+            # Load it into the InserTable and test this
+            with TemporaryDirectory() as tmp_dir:
+                with LowLevelConnection(1, os.path.join(tmp_dir, 'test.db')) as conn:
+                    with InsertTable(conn, data_frame) as insert_table:
+
+                        data_frame_size = len(data_frame.index)
+                        database_rows = conn.cursor() \
+                            .execute('SELECT count(*) FROM {}.{}'.format(insert_table[0], insert_table[1])).fetchall()[0][0]
+
+                        self.assertEqual(data_frame_size, database_rows, 'not all (or more?) rows written to database')
+
+        run_test(os.path.join('pynance', 'test_data', 'dkb_cash_sample.csv'), SupportedCsvTypes.DKBCash)
+        run_test(os.path.join('pynance', 'test_data', 'dkb_visa_sample.csv'), SupportedCsvTypes.DKBVisa)
 
 
 def test_suite():
     "return the test suite"
+
     suite = unittest.TestSuite()
+
     suite.addTest(LowLevelConnectionTestCase('test_creates_database_file_if_not_exists'))
     suite.addTest(LowLevelConnectionTestCase('test_opens_connection'))
     suite.addTest(LowLevelConnectionTestCase('test_creates_expected_tables'))
+
+    suite.addTest(InsertTableTestCase('test_create_temp_table_table_exists'))
+    suite.addTest(InsertTableTestCase('test_create_temp_table_choses_other_table_if_exists'))
+    suite.addTest(InsertTableTestCase('test_it_removes_the_temporary_table'))
+    suite.addTest(InsertTableTestCase('test_it_works_with_dataframes_from_text_importer'))
+
     return suite
