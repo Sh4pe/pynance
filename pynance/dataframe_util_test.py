533cb9bd4b77f92b42f846f363d8734183c61173
Fabian Meyer <fabian.meyer@mailbox.org>
2019-05-23 00:27:13 +0200

    extended dataframe hash util tests

--- pynance/dataframe_util_test.py
+++ pynance/dataframe_util_test.py
@@ -13,4 +13,13 @@ class DataframeUtilTestcase(unittest.TestCase):
         for i, row in df.iterrows():
             hash_result = hash_row(row)
             self.assertEqual(type(hash_result), str)
-            self.assertEqual(type(hash_result), str)
+            self.assertEqual(len(hash_result), 32)
+
+    @given(df=dataframe(min_size=1, max_date=date(2000, 1, 1)))
+    def test_create_id_hash(self, df):
+        result_hash_column = create_id_hash(df)
+        self.assertEqual(len(result_hash_column), len(df))
+
+        for item in result_hash_column:
+            self.assertEqual(type(item), str)
+            self.assertEqual(len(item), 32)
